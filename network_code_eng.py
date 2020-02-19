72e7417b0c903f3cdba410cd9c97189ceefda0c8
Ivan Chanke <ch.vanya@yandex.ru>
2020-02-19 16:18:53 +0300

    Translation finished. Code made cleanner. No fuctional changes

--- network_code_eng.py
+++ network_code_eng.py
@@ -1,11 +1,12 @@
+"""
+(C) Ivan Chanke 2020
+Contains classes:
+    Node - > Layer -> Network
+The three constantly refer to one another and aren't supposed to work separately
+For details on math model visit GitHub directory corresponding to the project
+"""
 import numpy as np
-"""
-CURRENT PARAMETERS:
-    WEIGHTS RANGE (-1;1)
-    BIAS RANGE (-1;1)
 
-Sigmoid and its derivative are defined below
-"""
 
 bias_signal = 1
 
@@ -21,8 +22,7 @@ def s_df(x):
 
 class Node:
     """
-    A basic node
-    Contains:
+    Stores:
         Its position in the network (position, layernum)
         A vector of weights going out of it.
         Local gradient
@@ -53,16 +53,12 @@ class Node:
         self.weights_delta = np.zeros(layer.nnodes)
 
     def apply_deltas(self):
-        """
-        Applies weights' deltas
-        """
+
         self.weights += self.weights_delta
         self.weights_delta = np.zeros(self.weights_delta.shape)
 
     def modify_bias(self):
-        """
-        Applies changes to bias weight
-        """
+
         self.bias_weight += self.bias_weight_delta
         self.bias_weight_delta = 0
 
@@ -70,15 +66,13 @@ class Node:
 class Layer:
     """
     Handles backpropagation and feeding forward.
-    Contains:
+    Stores:
         Layer index
         A list of nodes
         Number of nodes in the list (nnodes)
     """
     def __init__(self, nnodes, number):
-        """
-        Nnodes are initialized in "nodes" list
-        """
+
         self.number = number
         self.nnodes = nnodes
         self.nodes = [Node((i, number)) for i in range(nnodes)]
@@ -114,115 +108,82 @@ class Layer:
 
     def propagate_forward(self, other, vector):
         """
-        Отображает входной вектор слоя self во входной вектор для СЛЕДУЮЩЕГО слоя other
-        Также отвечает за применение сдвига в нейронах слоя other
+        Maps input vector of layer i to input vector of layer i+1
+        """
+        bias_weights_vector = np.array([node.bias_weight for node in other.nodes]) # Composes a vector of bias weigths for layer other
 
-        Составляет вектор весов сдвига для слоя other
-        """
-        bias_weights_vector = np.array([node.bias_weight for node in other.nodes])
-        """
-        Собирает матрицу весов из весов каждого собственного нейрона
-        Добавляет вектор весов сдвига следующего слоя
-        Транспонирует матрицу так, что каждая колонка - вектор весов
-        """
         weights_stack = [node.weights for node in self.nodes]
         weights_stack.append(bias_weights_vector)
 
-        memory_matrix = np.vstack(weights_stack) # Stacks up weight vectors of each node in the self and bias weight vector of other
+        memory_matrix = np.vstack(weights_stack) # Stacks up weight vectors of each node in self and bias weight vector of other
         memory_matrix = np.transpose(memory_matrix) # Each column is a weight vector; ncols = nnodes + 1, nrows + 1 = nsynapses;
-        """
-        Каждый нейрон запоминает своё индуцированное поле
-        Оно пригодится во время обратного распространения
-        """
+
         for i in range(len(self.nodes)):
-            self.nodes[i].induced_field = vector[i] # Each node stores its induced field later used to compute local_grad
-        """
-        Добавляем входному вектору сигнал от сдвига
-        """
-        vector = sigmoid(vector) # vector goes through the activation function
+            self.nodes[i].induced_field = vector[i] # Each node stores its induced field which is later used to compute local_grad
+
+        vector = sigmoid(vector) # Vector goes through the activation function
         vector = np.append(vector, bias_signal) # Bias signal is added to a vector
 
-        return (memory_matrix.dot(vector)) # Returns input vector for the next layer
+        return memory_matrix.dot(vector) # Returns input vector for the next layer
 
     def start_backpropagation(self, e, learning_rate): # For output layer only; computes local_grad for each node in self; e - error vector
         """
-        Начинает обратное распространение
-        Вычисляет локальные градиенты нейронов выходного слоя
+        Computes a local gradient for each neuron in the output vector
         """
         for i in range(len(self.nodes)):
             self.nodes[i].local_grad = s_df(self.nodes[i].induced_field) * e[i]
 
             self.nodes[i].bias_weight_delta += learning_rate * self.nodes[i].local_grad * bias_signal
 
-
-
     def propagate_backward(self, other, learning_rate): # For layers except output only; connection scheme: self-other
         """
-        Обратное распространение через один слой в направлении от other
-        к self.
-        """
-        # This method computes local grads for nodes in self. Then it uses local grads in other to compute deltas.
-        """
-        Подсчёт локального градиента каждого нейрона слоя self
+        Computes local gradients for nodes in self
+
         """
         for i in range(len(self.nodes)): # Local_grad for each node in layer self is computed
             lgv = np.array([node.local_grad for node in other.nodes]) # Local gradient vector for other
             d = s_df(self.nodes[i].induced_field) * np.dot(lgv, np.transpose(self.nodes[i].weights))
             self.nodes[i].local_grad = d
-
         """
-        Вычисляются поправки к весам и сдвигу
+        Computes deltas using local gradients
         """
-        #print('Проход через слой {}'.format(self.number))
         for node in self.nodes:
             deltas = []
             for i in range(len(node.weights)):
                 delta = learning_rate * sigmoid(node.induced_field) * other.nodes[i].local_grad
                 deltas.append(delta)
-            #print(node.bias_weight)
+
             bias_delta = learning_rate * node.local_grad * bias_signal
-            """
-            Вычисленные поправки учитываются в общей поправке эпохи
-            """
+
             node.weights_delta += np.array(deltas)
             node.bias_weight_delta += bias_delta
 
 
-
-
-
 class Network:
     """
-    Класс сети
+    Stores:
+        Current signal
+        A list of layers
+    Initializing a network also initializes its layers and nodes in them
     """
     def __init__(self, structure): # Structure is a tuple of nnodes in each layer
-        """
-        Сеть хранит:
-            Текущий сигнал, который через неё проходит
-            Список слоёв
-        При инициализации сети инициализируются слои, в каждом слое
-        инициализируются нейроны. Слои соединяются. Инициализируются сдвиги.
-        """
-        self.signal = None # The vector currently going through the network
+
+        self.signal = None
         self.layers = []
 
         for i in range(len(structure)): # Constructing layers
             self.layers.append(Layer(structure[i], i))
 
-        for i in range(1, len(self.layers)): # Connecting layers; first "other" - layer[0], last "self" - layer[-1]
+        for i in range(1, len(self.layers)):
             self.layers[i].connect_layer(self.layers[i - 1])
 
         for i in range(1, len(self.layers)): # Initializing bias weights
             for j in range(len(self.layers[i].nodes)):
                 self.layers[i].nodes[j].bias_weight = (np.random.rand()) * 2 - 1
-                print('Слой {} нейрон {} сдвиг инициализирован'.format(i, j))
 
-
-
-    def feed_forward(self, vector): # Maps input-output. Last layer is handled separately
+    def feed_forward(self, vector):
         """
-        Отображает входной вектор в выходной
-        Метод ссылается на методы прогонки сигнала вперед класса "Слой"
+        Maps input-output
         """
         self.signal = self.layers[0].start_forward_propagation(self.layers[1], vector)
 
@@ -234,10 +195,10 @@ class Network:
 
         return self.signal
 
-    def feed_backward(self, error_vector, learning_rate): # Recursively computes deltas for weights; applies them
+    def feed_backward(self, error_vector, learning_rate):
         """
-        Обратное распространение ошибки
-        Также ссылается на методы класса "Слой"
+        Backpropagation
+        Recursively computes deltas for weights; applies them
         """
         self.layers[-1].start_backpropagation(error_vector, learning_rate)
 
@@ -245,34 +206,31 @@ class Network:
             self.layers[i - 1].propagate_backward(self.layers[i], learning_rate)
 
 
-    def learning_iteration(self, batch_tuple, learning_rate): # batch_tuple is a tuple of tuples: ((in, desired_out), (...), ..., (...))
+    def learning_iteration(self, batch_tuple, learning_rate):
         """
-        Одна эпоха обучения.
-        Прогоняет вперёд каждый пример, состоящий из пары векторов вход-выход
-        Для каждого примера прогоняет назад ошибку.
-        В конце применяет поправки в весам и сдвигам.
+        One complete learning epoch
+        batch_tuple is a tuple of tuples: ((in, desired_out), (...), ..., (...))
         """
         mse = 0
-        for instance in batch_tuple: # Each instance in the batch has its effect on the total error because it wants different outputs
-            output = sigmoid(self.feed_forward(np.array(instance[0]))) # Feeds forward the "in" vector
+        for instance in batch_tuple:
+            output = sigmoid(self.feed_forward(np.array(instance[0])))
             error_vector = instance[1] - output
             mse += sum(error_vector**2)
 
             self.feed_backward(error_vector, learning_rate)
 
-            #print(instance[0], ':', output, "| e:", error_vector) # Печатает полученный на выходе сигнал
+            #print(instance[0], ':', output, "| e:", error_vector)
             print(instance[0], ':', output)
-
 
         for node in self.layers[0].nodes:
             node.apply_deltas()
+
         for i in range(1, len(self.layers) - 1):
             for node in self.layers[i].nodes:
-                #print('bias', node.bias_weight)
                 node.apply_deltas()
                 node.modify_bias()
+
         for node in self.layers[-1].nodes:
-                #print('bias_last', node.bias_weight)
                 node.modify_bias()
 
         #print('MSE:', mse)
